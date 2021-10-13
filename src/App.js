0085edfda772edf081a511323f5acf88c3572e0c
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-13 18:57:48 +0530

    Add Pet component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, "Luna"),
+    React.createElement("h3", {}, "Dog"),
+    React.createElement("h3", {}, "German Sheperd"),
+  ]);
+};
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!")
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet),
+  ]);
 };
 ReactDOM.render(React.createElement(App), document.getElementById("root"));

c3b27a98a7e3c938c7a6019bfa68a43dcd791d9f
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-14 10:13:26 +0530

    Api request using useEffect

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle,WA");
-  const [animal, setAnimal] = useState("");
+  const [location, setLocation] = useState("india");
+  const [animal, setAnimal] = useState(ANIMALS[0]);
   const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
   const breeds = [];
+  useEffect(() => {
+    requestPets();
+  }, [animal]);
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+    console.log(json);
+
+    setPets(json.pets);
+  }
+
   return (
     <div className="search-params">
       <form>
@@ -51,6 +66,14 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };

71246484310feb45fd660b708727cd6a7762e6eb
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-14 09:32:55 +0530

    Breed Array

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -2,7 +2,9 @@ import { useState } from "react";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle,WA");
-
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
   return (
     <div className="search-params">
       <form>
@@ -16,14 +18,36 @@ const SearchParams = () => {
           />
         </label>
         <label htmlFor="animal">
-          {" "}
-          Animal{" "}
+          Animal
           <select
             id="animal"
             value={animal}
             onChange={(e) => setAnimal(e.target.value)}
             onBlur={(e) => setAnimal(e.target.value)}
-          ></select>
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="animal"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option value={breed} key={breedl}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>

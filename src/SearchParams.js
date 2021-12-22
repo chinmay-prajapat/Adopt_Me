60fdb7ef74f0426e8ab2e791d2003b15451d030f
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-12-22 11:37:16 +0530

    useEffect hook request

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
+import Pet from "./Pet";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
+  const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
@@ -68,9 +69,14 @@ const SearchParams = () => {
         </label>
         <button type="submit">Submit</button>
       </form>
-      {
-        pets.map(pet=>())
-      }
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };

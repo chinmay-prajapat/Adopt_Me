601fc92afbd19a1a127d16797534795c4363f7af
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-12-22 16:39:52 +0530

    Router install

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 import SearchParams from "./SearchParams";
-
+import Details from "./Details";
+import { BrowserRouter as Router, Rout } from "react-router-dom";
 const App = () => {
   return (
     <div>
       <h1 id="my-brand">Adop-Me</h1>
-      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" /> */}
+
       <SearchParams />
     </div>
   );

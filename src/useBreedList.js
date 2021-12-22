99fb93cf0e4c26d4c02b3eda949269ba123c313b
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-12-22 12:14:45 +0530

    refactoring pet component

---
+++ src/useBreedList.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+const localCache = {};
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
+  });
+}
